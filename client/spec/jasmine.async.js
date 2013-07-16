7b9b72e8676f40cd27c593786d3368bea39665f5
traviswimer <TravisWimer@Earth>
2013-07-16 16:03:43 -0400

    Adds custom timeouts for async tests

---
+++ client/spec/jasmine.async.js
@@ -0,0 +1,59 @@
+// Jasmine.Async, v0.1.0
+// Copyright (c)2012 Muted Solutions, LLC. All Rights Reserved.
+// Distributed under MIT license
+// http://github.com/derickbailey/jasmine.async
+//
+// (This script is not the same as the the version in the GIThub repo.
+// This version allows for custom max wait times for asynch test)
+
+this.AsyncSpec = (function(global){
+
+  // Private Methods
+  // ---------------
+
+  function runAsync(block, timeout){
+    return function(){
+      var done = false;
+      var complete = function(){ done = true; };
+
+      runs(function(){
+        block(complete);
+      });
+
+      waitsFor(
+        function(){
+          return done;
+        },
+        null,
+        timeout
+      );
+    };
+  }
+
+  // Constructor Function
+  // --------------------
+
+  function AsyncSpec(spec){
+    this.spec = spec;
+  }
+
+  // Public API
+  // ----------
+
+  AsyncSpec.prototype.beforeEach = function(block){
+    this.spec.beforeEach(runAsync(block));
+  };
+
+  AsyncSpec.prototype.afterEach = function(block){
+    this.spec.afterEach(runAsync(block));
+  };
+
+  AsyncSpec.prototype.it = function(description, block, timeout){
+    // For some reason, `it` is not attached to the current
+    // test suite, so it has to be called from the global
+    // context.
+    global.it(description, runAsync(block, timeout));
+  };
+
+  return AsyncSpec;
+})(this);
